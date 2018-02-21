0396d631c1a8471f44ba65fdbe175fe25f94532c
wma <wma@thoughtworks.com>
2018-02-21 11:58:51 +0800

    add test cases for auth process

--- src/test/java/com/zhiliao/api/zhiliaoapi/controllers/AuthControllerTest.java
+++ src/test/java/com/zhiliao/api/zhiliaoapi/controllers/AuthControllerTest.java
@@ -8,15 +8,20 @@ import com.zhiliao.api.zhiliaoapi.utils.SecurityHelper;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.security.NoSuchAlgorithmException;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import static org.springframework.http.HttpStatus.OK;
 
 public class AuthControllerTest extends ControllerTestBase {
+    private final String AUTH_URL = "/auth";
     @Autowired
     private UserDAO userDAO;
+    private final String MOBILE = "13900001111";
+    private final String PASSWORD = "123";
 
     @Before
     public void setUp() throws Exception {
@@ -25,19 +30,50 @@ public class AuthControllerTest extends ControllerTestBase {
 
     @Test
     public void shouldLoginSuccessfully() throws Exception {
-        String mobile = "13900001111";
-        String password = "123";
-
-        userDAO.create(mobile, SecurityHelper.hash(password));
+        createUser();
 
         AuthRequest authRequest = new AuthRequest();
-        authRequest.setMobile(mobile);
-        authRequest.setPassword(password);
+        authRequest.setMobile(MOBILE);
+        authRequest.setPassword(PASSWORD);
 
-        ResponseEntity<AuthResponse> authResponse = restTemplate.postForEntity("/auth", authRequest, AuthResponse.class);
+        ResponseEntity<AuthResponse> authResponse = restTemplate.postForEntity(AUTH_URL, authRequest, AuthResponse.class);
 
         assertThat(authResponse, not(nullValue()));
-        assertThat(authResponse.getStatusCode(), is(HttpStatus.OK));
-        assertThat(authResponse.getBody().getMobile(), is(mobile));
+        assertThat(authResponse.getStatusCode(), is(OK));
+        assertThat(authResponse.getBody().isLoggedIn(), is(true));
+        assertThat(authResponse.getBody().getMobile(), is(MOBILE));
+        assertThat(authResponse.getBody().getToken(), not(nullValue()));
+    }
+
+    @Test
+    public void shouldLoginFailedGivenWrongMobile() throws Exception {
+        createUser();
+
+        AuthRequest authRequest = new AuthRequest("wrong mobile", PASSWORD);
+        ResponseEntity<AuthResponse> authResponse = restTemplate.postForEntity(AUTH_URL, authRequest, AuthResponse.class);
+
+        assertThat(authResponse.getStatusCode(), is(OK));
+        assertThat(authResponse.getBody().isLoggedIn(), is(false));
+        assertThat(authResponse.getBody().getToken(), is(nullValue()));
+    }
+
+    @Test
+    public void shouldLoginFailedGivenWrongPassword() throws Exception {
+        createUser();
+
+        AuthRequest authRequest = new AuthRequest(MOBILE, "wrong password");
+        ResponseEntity<AuthResponse> authResponse = restTemplate.postForEntity(AUTH_URL, authRequest, AuthResponse.class);
+
+        assertThat(authResponse.getStatusCode(), is(OK));
+        assertThat(authResponse.getBody().isLoggedIn(), is(false));
+        assertThat(authResponse.getBody().getToken(), is(nullValue()));
+    }
+
+    private void createUser() throws NoSuchAlgorithmException {
+        createUser(MOBILE, PASSWORD);
+    }
+
+    private void createUser(String mobile, String password) throws NoSuchAlgorithmException {
+        userDAO.create(mobile, SecurityHelper.hash(password));
     }
 }
